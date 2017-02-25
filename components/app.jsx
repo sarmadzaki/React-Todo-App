7895b31a12e25fda50d847fb2bef935f62c065a4
sam <sarmad.zaki@yahoo.com>
2017-02-25 17:24:00 +0500

    All

--- components/app.jsx
+++ components/app.jsx
@@ -4,7 +4,14 @@ import { Provider } from 'react-redux'
 import store from '../store'
 import Todos from './todos'
 import * as firebase from 'firebase';
- 
+ var config = {
+    apiKey: "AIzaSyDcAn4JEiICAeBeCbpnLcYEors4sf6mjyw",
+    authDomain: "sam-todo-react.firebaseapp.com",
+    databaseURL: "https://sam-todo-react.firebaseio.com",
+    storageBucket: "sam-todo-react.appspot.com",
+    messagingSenderId: "727433514972"
+  };
+firebase.initializeApp(config);
 let reactElement = document.getElementById('react')
 render(
   <Provider store={store}>
